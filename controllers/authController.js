113bfa2b0f868b220cff790d75ea0324a6afcf50
Janvinsha <janvinsha@gmail.com>
2021-03-18 19:57:34 +0100

    commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,8 +17,8 @@ const createSendToken = (user, statusCode, res,req) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true,
-    secure:req.secure||req.headers('x-forwarded-proto')==="https"
+    httpOnly: false,
+    secure:false
   });
   user.password = undefined;
   res.status(statusCode).json({
